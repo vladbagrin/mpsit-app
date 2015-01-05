c6ec64a8db8b631165f51bd533346ed78b3be3af
Vlad Bagrin <vlad.bagrin@gmail.com>
2015-01-06 00:08:20 +0200

    added the renamed files

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php		
+require_once("global.php");
+require_once("facebook.php");
+require_once("utils.php");
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Birthday Thanker</title>
+ 
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.3.0/build/cssreset/reset-min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
+</head>
+ 
+<body background="images/wallpaper.jpg">
+<div class="wrapper">
+	<div> </br> </div>
+	
+    <div class="maincontent">
+			<div class="logo"><img src="images/thankyou2.png" width="379" height="128" alt="thankyou logo" a href="index.html"></div>
+			 
+		
+        <div class="tab_container">
+			<div class="pagina"> 
+				<div class="line">
+					<form action="detect.php" method="post" class="tile_form">
+						<input type="hidden" name="signed_request" value="<?=$_REQUEST['signed_request']?>">
+						<button class="tile tileLarge verde" name='type' type="submit" value="detect"> 
+							</br> Comment by detecting language 
+						</button>
+					</form>
+					<form action="choose.php" method="post" class="tile_form">
+						<input type="hidden" name="signed_request" value="<?=$_REQUEST['signed_request']?>">
+						<button class="tile tileLarge vermelho" name='type' type="submit" value="choose">					
+							</br> Comment by choosing language	
+						</button>
+					</form>
+				</div> 
+				<div class="line">
+					<form action="write.php" method="post" class="tile_form">
+						<input type="hidden" name="signed_request" value="<?=$_REQUEST['signed_request']?>">
+						<button class="tile tileLarge azul" name='type' type="submit" value="write"> 
+							</br> Write your own comments
+						</button>
+					</form>
+					<form action="like.php" method="post" class="tile_form">
+						<input type="hidden" name="signed_request" value="<?=$_REQUEST['signed_request']?>">
+						<button class="tile amarelo" name='type' type="submit" value="like"> 
+							</br> Like all posts
+						</button>
+					</form>
+					<form action="about.php" method="post" class="tile_form">
+						<input type="hidden" name="signed_request" value="<?=$_REQUEST['signed_request']?>">
+						<button class="tile coral" name='type' type="submit" value="about"> 
+							</br> About
+						</button>
+					</form>
+				</div>
+			</div>
+        </div><!--End Tab Container -->
+     </br></br></br>
+	<div class="footbar">
+	TODO:Facebook like/comment bar
+    </div><!--End Sidebar-->
+	
+     </div><!--End Main Content-->
+ 
+</div><!--End Wrapper -->
+ 
+ 
+</body>
+</html>
---
+++ like.php
@@ -0,0 +1,50 @@
+<?php		
+require_once("global.php");
+require_once("facebook.php");
+require_once("utils.php");
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Birthday Thanker</title>
+ 
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.3.0/build/cssreset/reset-min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
+</head>
+ 
+<body background="images/wallpaper.jpg">
+<div class="wrapper">
+	<INPUT Type="BUTTON" class="btn" VALUE="Home Page" ONCLICK="top.location.href='index.php'"> 
+    <div class="maincontent">
+			<div class="logo"><img src="images/thankyou2.png" width="379" height="128" alt="thankyou logo" a href="index.php"></div>
+        <div class="tab_container">
+		<div id="user_info">
+		<div id="picture">
+			<img src="<?php echo $_SESSION['PICTURE']; ?>"> </br>
+		</div>
+		<div id="birthday">
+			<?php echo $_SESSION['BIRTHDAY']; ?>
+		</div>
+		</div>		
+		<?php
+					// session originates in facebook.php
+			if ($session) {
+				getLikeAllPostsOutput();
+			}
+		?>
+        </div><!--End Tab Container -->
+     
+     </div><!--End Main Content-->
+     
+    <div class="sidebar">
+    </div><!--End Sidebar-->
+ 
+</div><!--End Wrapper -->
+ 
+ 
+</body>
+</html>
